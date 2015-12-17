22ec8bf1da35d67b98881a3440dda9dd991c42b9
Jesse Lucas <jesse@forestgiant.com>
2015-12-16 21:57:49 -0500

    Creating pre and post command hooks.

--- r.sh
+++ r.sh
@@ -1,26 +1,38 @@
 #!/bin/bash
 
-_r() {
-  echo "test"
+# This will run before any command is executed.
+function pre() {
+  if [ -z "$R_AT_PROMPT" ]; then
+    return
+  fi
+  unset R_AT_PROMPT
 
-  # keep reference to what command was executed
-  local cmd="${1##*/}"
+  # Verify command
 
-  # Add command and path to r
-  r --add "$(\pwd)":$cmd
 
-  # Execute command
-  "$cmd"
+  # Add current directory and command to `r`
+  echo "Running PreCommand"
+
+  r --add "$(\pwd)"
 }
 
-# Look for existing aliases
-array=($(alias))
-echo "${array[@]}"
+# Set trap to reun pre before command
+trap "pre" DEBUG
 
-# List of commands to alias list is returned from `r --commands`
+# This will run after the execution of the previous full command line.  We don't
+# want `post` to execute when first starting a bash session (FIRST_PROMPT)
+R_FIRST_PROMPT=1
+function post() {
+  R_AT_PROMPT=1
 
-# Loop through list of commands, and check against existing alias
+  if [ -n "$R_FIRST_PROMPT" ]; then
+    unset R_FIRST_PROMPT
+    return
+  fi
 
-# alias all commands
-alias ls='_r ls'
-alias pwd='_r pwd'
+  # Post command logic
+  echo "Running PostCommand"
+}
+
+# Run post after command
+PROMPT_COMMAND="$PROMPT_COMMAND"$'\n''post'
