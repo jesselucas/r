2acadb18f5dd25a072c155baf6d4fb8954c13912
Jesse Lucas <jesse@forestgiant.com>
2015-12-18 14:06:57 -0500

    Setting up boltdb to persisent data

--- main.go
+++ main.go
@@ -6,11 +6,16 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"os/user"
+	"path/filepath"
 	"strings"
 	"sync"
 
+	"github.com/boltdb/bolt"
 	"github.com/chzyer/readline"
 )
+
+var db *bolt.DB
 
 func main() {
 	// Setup flags
@@ -19,6 +24,19 @@ func main() {
 	commandPtr := flag.Bool("command", false, "show last command selected from `r`")
 	addPtr := flag.String("add", "", "show stats and usage of `r`")
 	flag.Parse()
+
+	// Setup bolt db
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+	boltPath := filepath.Join(usr.HomeDir, ".r.db")
+	// It will be created if it doesn't exist.
+	db, err = bolt.Open(boltPath, 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
 	// Check if `results` flag is passed
 	// if *completePtr != "" {
@@ -30,7 +48,7 @@ func main() {
 	// }
 
 	if *commandPtr {
-		fmt.Println(`echo "it worked!!"`)
+		printLastCommand()
 		os.Exit(0)
 	}
 
@@ -50,7 +68,11 @@ func main() {
 
 func readLine() {
 	// create completer from results
-	results := showResults("r")
+	results, err := showResults()
+	if err != nil {
+		log.Panic(err)
+	}
+
 	var pcItems []*readline.PrefixCompleter
 	for _, result := range results {
 		pcItems = append(pcItems, readline.PcItem(result))
@@ -73,36 +95,75 @@ func readLine() {
 		}
 
 		// set line as stored command
-		fmt.Println(line)
+		err = db.Update(func(tx *bolt.Tx) error {
+			b, err := tx.CreateBucketIfNotExists([]byte("command"))
+			if err != nil {
+				return err
+			}
+
+			err = b.Put([]byte("command"), []byte(line))
+			if err != nil {
+				return err
+			}
+			return nil
+		})
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		// fmt.Println(line)
 		os.Exit(0)
 	}
 }
 
-// setupDB verifies and creates boltDB in ~ home folder
-func setupDB() error {
-	// TODO setup boltdb
-	fmt.Println("setup boltdb")
+func printLastCommand() {
+	var val string
+	db.Update(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucketIfNotExists([]byte("command"))
+		if err != nil {
+			return err
+		}
 
-	return nil
+		val = string(b.Get([]byte("command")))
+		return nil
+	})
+
+	fmt.Println(val)
 }
 
-func showResults(input string) []string {
-	results := []string{"git status", "git clone", "go install", "cd /Users/jesse/", "cd /Users/jesse/gocode/src/github.com/jesselucas", "ls -Glah"}
-
-	if input == "r" {
-		return results
+func showResults() ([]string, error) {
+	wd, err := os.Getwd()
+	if err != nil {
+		return nil, err
 	}
+
+	// results := []string{"git status", "git clone", "go install", "cd /Users/jesse/", "cd /Users/jesse/gocode/src/github.com/jesselucas", "ls -Glah"}
+	var results []string
+	err = db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("DirectoryBucket"))
+		pathBucket := b.Bucket([]byte(wd))
+		return pathBucket.ForEach(func(k, v []byte) error {
+			fmt.Printf("key=%s, value=%s\n", k, v)
+			results = append(results, string(k))
+			return nil
+		})
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return results, nil
 
 	// filter
-	fmt.Println("filtered: ", input)
-	var filtered []string
-	for _, result := range results {
-		if strings.HasPrefix(result, input) {
-			filtered = append(filtered, result)
-		}
-	}
-
-	return filtered
+	// var filtered []string
+	// for _, result := range results {
+	// 	if strings.HasPrefix(result, input) {
+	// 		filtered = append(filtered, result)
+	// 	}
+	// }
+	//
+	// return filtered
 
 }
 
@@ -124,8 +185,38 @@ func add(path string, promptCmd string) error {
 
 	// Add command to db
 	fmt.Printf("adding. cmd: %s, path: %s \n", promptCmd, path)
+	return db.Update(func(tx *bolt.Tx) error {
+		directoryBucket, err := tx.CreateBucketIfNotExists([]byte("DirectoryBucket"))
+		if err != nil {
+			return err
+		}
 
-	return nil
+		pathBucket, err := directoryBucket.CreateBucketIfNotExists([]byte(path))
+		if err != nil {
+			return err
+		}
+
+		// Store path and command for contextual path sorting
+		cmdBucket, err := tx.CreateBucketIfNotExists([]byte("CommandBucket"))
+		if err != nil {
+			return err
+		}
+
+		// Don't store if the command is r
+		if cmd == "r" {
+			return nil
+		}
+
+		// TODO first look up command and then increment it's count
+
+		// Store just command and usage for overall most used sorting
+		err = cmdBucket.Put([]byte(promptCmd), []byte("1"))
+		if err != nil {
+			return err
+		}
+
+		return pathBucket.Put([]byte(promptCmd), []byte("1"))
+	})
 }
 
 func containsCmd(cmd string, commands []string) bool {
