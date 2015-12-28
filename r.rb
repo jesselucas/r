ceafa905c5f2dadab24de307b9059c90453e7ba0
Jesse Lucas <jesse@forestgiant.com>
2015-12-28 17:37:12 -0500

    Updating homebrew formula

---
+++ r.rb
@@ -0,0 +1,29 @@
+class R < Formula
+  desc "r is a contextual, path based, bash history."
+  homepage "https://jesselucas.github.io/r/"
+  url "https://github.com/jesselucas/r/archive/0.3.1.tar.gz"
+  version "0.3.1"
+  sha256 "57dabcabb46bacc3a263af13cc23d0b0514687a4a4da82e9704a4b1f29bfd31e"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    ENV["GO15VENDOREXPERIMENT"] = "1"
+
+    (buildpath/"src/github.com/jesselucas/r/").install Dir["*"]
+    system "go", "build", "-o", "#{bin}/r", "-v", "github.com/jesselucas/r/"
+  end
+
+
+  test do
+    actual = system("r -install")
+    expected = true
+    actualVersion = pipe_output("#{bin}/r -version")
+    # assert_equal expected, actual
+    assert_block do
+      actual == expected
+      actualVersion == version
+    end
+  end
+end
